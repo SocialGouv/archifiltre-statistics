d07dddab1cfbf8a71a2aebf85c4bee51c274b278
Clément Berthou <clement.berthou@benextcompany.com>
2020-12-09 01:34:52 +0100

    chore: remove environment/*/app.ts files

--- .k8s/environments/dev/app.ts
+++
@@ -1,7 +0,0 @@
-import { GlobalEnvironment } from "@socialgouv/kosko-charts/types";
-
-export default {
-  subdomain: `${process.env.CI_COMMIT_REF_SLUG}-${
-    process.env.CI_PROJECT_NAME as string
-  }`,
-} as Partial<GlobalEnvironment>;
