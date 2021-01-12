29784993c6e092acf82485688dd68e3277da7025
Benjamin Morali <benjaminmorali4@hotmail.fr>
2021-01-12 11:34:01 +0100

    feat(cache): add caching to statistics (#22)

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,14 @@ import express from "express";
 import { flatten } from "lodash/fp";
 
 import packageJson from "../package.json";
+import { createCache } from "./caching/caching-service";
 import { corsOrigins, port } from "./config";
 import { getGitHubData } from "./github/github-service";
 import { matomoConfig } from "./matomo/matomo-config";
 import { getMultiSiteMatomoData } from "./matomo/matomo-service";
 import { getYoutubeData } from "./youtube/youtube-service";
+
+const CACHE_TTL = 10 * 60 * 1000;
 
 const app = express();
 
@@ -25,14 +28,28 @@ app.get("/healthz", (req, res) => {
   res.send("OK");
 });
 
+const statsCache = createCache(
+  async () =>
+    Promise.all([
+      getMultiSiteMatomoData(matomoConfig),
+      getYoutubeData(),
+      getGitHubData(),
+    ]).then(flatten),
+  CACHE_TTL
+);
+
 app.get("/statistics", (req, res) => {
-  void Promise.all([
-    getMultiSiteMatomoData(matomoConfig),
-    getYoutubeData(),
-    getGitHubData(),
-  ])
-    .then(flatten)
-    .then((data) => res.json(data));
+  res.json({
+    lastFetchTimestamp: statsCache.getLastFetchTimestamp(),
+    result: statsCache.read(),
+  });
+});
+
+app.post("/refresh", (req, res) => {
+  void statsCache
+    .refresh()
+    .then(() => res.status(204).send())
+    .catch(() => res.status(500).send("Error refreshing cache"));
 });
 
 app.listen(port, () => {
