dc0ee0e582ccf79c286c505a3adf4d9cd55e4a24
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-09-10 13:33:50 +0200

    fix: Prevent caching-service.test.ts from hanging.

--- src/caching/caching-service.ts
+++ src/caching/caching-service.ts
@@ -6,22 +6,16 @@ type CacheObject<T> = {
 
 type FetchData<T> = () => Promise<T> | T;
 
-export const createCache = <T>(
-  fetchData: FetchData<T>,
-  ttl: number
-): CacheObject<T> => {
+export const createCache = <T>(fetchData: FetchData<T>): CacheObject<T> => {
   let cacheData: T | null = null;
   let lastFetchTimestamp = 0;
-  let refreshTimeout: NodeJS.Timeout | null = null;
   const refresh = async () => {
-    refreshTimeout?.unref();
     try {
       cacheData = await fetchData();
       lastFetchTimestamp = Date.now();
     } catch (error: unknown) {
       console.error(error);
     }
-    refreshTimeout = setTimeout(() => void refresh(), ttl);
   };
   void refresh();
   return {
