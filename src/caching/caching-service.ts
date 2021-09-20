b15f924d72a2638d4dd5c67bb7c6bcfbe48b2f15
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-09-20 12:20:32 +0200

    fix: Clean code

--- src/caching/caching-service.ts
+++ src/caching/caching-service.ts
@@ -30,30 +30,3 @@ export const createCache = <T>(
     refresh,
   };
 };
-
-// type CacheObject<T> = {
-//   read: () => T | null;
-//   refresh: () => Promise<void>;
-//   getLastFetchTimestamp: () => number | null;
-// };
-
-// type FetchData<T> = () => Promise<T> | T;
-
-// export const createCache = <T>(fetchData: FetchData<T>): CacheObject<T> => {
-//   let cacheData: T | null = null;
-//   let lastFetchTimestamp = 0;
-//   const refresh = async () => {
-//     try {
-//       cacheData = await fetchData();
-//       lastFetchTimestamp = Date.now();
-//     } catch (error: unknown) {
-//       console.error(error);
-//     }
-//   };
-//   void refresh();
-//   return {
-//     getLastFetchTimestamp: () => lastFetchTimestamp,
-//     read: () => cacheData,
-//     refresh,
-//   };
-// };
