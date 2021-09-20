ef24482ebc3e11bcb34a315c21f7e6c79d00cff4
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-09-20 12:10:05 +0200

    fix: Remove caching-service test

--- src/caching/caching-service.ts
+++ src/caching/caching-service.ts
@@ -6,16 +6,22 @@ type CacheObject<T> = {
 
 type FetchData<T> = () => Promise<T> | T;
 
-export const createCache = <T>(fetchData: FetchData<T>): CacheObject<T> => {
+export const createCache = <T>(
+  fetchData: FetchData<T>,
+  ttl: number
+): CacheObject<T> => {
   let cacheData: T | null = null;
   let lastFetchTimestamp = 0;
+  let refreshTimeout: NodeJS.Timeout | null = null;
   const refresh = async () => {
+    refreshTimeout?.unref();
     try {
       cacheData = await fetchData();
       lastFetchTimestamp = Date.now();
     } catch (error: unknown) {
       console.error(error);
     }
+    refreshTimeout = setTimeout(() => void refresh(), ttl);
   };
   void refresh();
   return {
@@ -24,3 +30,30 @@ export const createCache = <T>(fetchData: FetchData<T>): CacheObject<T> => {
     refresh,
   };
 };
+
+// type CacheObject<T> = {
+//   read: () => T | null;
+//   refresh: () => Promise<void>;
+//   getLastFetchTimestamp: () => number | null;
+// };
+
+// type FetchData<T> = () => Promise<T> | T;
+
+// export const createCache = <T>(fetchData: FetchData<T>): CacheObject<T> => {
+//   let cacheData: T | null = null;
+//   let lastFetchTimestamp = 0;
+//   const refresh = async () => {
+//     try {
+//       cacheData = await fetchData();
+//       lastFetchTimestamp = Date.now();
+//     } catch (error: unknown) {
+//       console.error(error);
+//     }
+//   };
+//   void refresh();
+//   return {
+//     getLastFetchTimestamp: () => lastFetchTimestamp,
+//     read: () => cacheData,
+//     refresh,
+//   };
+// };
